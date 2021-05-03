f8ec8458aab97d118441a0212d7cdb93f351c177
xiechuntao <306454568@qq.com>
2021-05-04 01:12:39 +0800

    学习一些基础知识

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <Home></Home>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
