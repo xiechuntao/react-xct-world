5eac87ad09b30770581525f103ce38646bd709e4
xiechuntao <306454568@qq.com>
2021-05-06 18:43:50 +0800

    提交一些变更

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.scss';
 import Home from './pages/Home'
 
+ 
 function App() {
   return (
     <div className="App">
@@ -10,24 +11,12 @@ function App() {
           target="_blank" rel="noopener noreferrer">
           <img src={logo} className="App-logo" alt="logo" />
         </a>
+ 
       </header>
       <main className="App-main">
         <Home />
       </main>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Go
-        </a>
-      </header> */}
+ 
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
-import './index.scss'
+import React, { Component } from 'react';
+import './index.scss';
+import Center from './Center';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="container">
-        <div class="center"></div>
+        <div className="center"></div>
         <div>Hello Home Page;</div>
-        <div></div>
+        <div>
+          <Center />
+        </div>
         <div></div>
         <div></div>
         <div></div>
