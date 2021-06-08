681c2418276391ed6f23f2facfd620e31d114d35
xiechuntao <306454568@qq.com>
2021-06-09 01:44:07 +0800

    提交一些变更

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -24,7 +24,7 @@ export default class Header extends Component {
       })
       .catch(error => {
         console.log(error);
-        thissetState({
+        this.setState({
           isLoaded: false,
           error: error,
         });
